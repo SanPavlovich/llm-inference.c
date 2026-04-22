1df0ffc76b054f677925eb4b3e7438cc004002cd
Alexander Kryazhenkov <a.kryazhenkov@vkteam.ru>
2026-04-22 21:02:24 +0300

    test llama attention

---
+++ tests/attention/test_attention.py
@@ -0,0 +1,31 @@
+import torch
+from transformers.models.llama.modeling_llama import LlamaAttention, LlamaConfig
+
+from .llama_attention import TransformerConfig, CausalSelfAttention
+
+
+torch.manual_seed(43)
+
+
+if __name__ == "__main__":
+    module = LlamaAttention()
+
+    custom_config = TransformerConfig(n_heads=4, n_kv_head=2, hidden_dim=8, intermediate_dim=32, dropout=0)
+    custom_block = CausalSelfAttention(custom_config)
+
+    
+
+    batch_size, seq_len, embed_dim = 2, 4, 8
+
+    tensor_input = torch.randn(batch_size, seq_len, embed_dim)
+    print(f"{tensor_input=}")
+
+    config = LlamaConfig(num_attention_heads=4, num_key_value_heads=2, hidden_size=embed_dim)
+    config._attn_implementation = "eager"
+    block = LlamaAttention(layer_idx=0, config=config)
+    print(block)
+
+
+    sin, cos = torch.randn(1), torch.randn(1)
+    tensor_output = block(tensor_input, position_embeddings=(sin, cos), attention_mask=None)
+    print(f"{tensor_output=}")
